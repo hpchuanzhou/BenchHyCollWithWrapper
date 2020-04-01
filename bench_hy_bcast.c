d18d107727d64da8b7eee16139367baf9a8d8724
huanzhou89 <33185600+huanzhou89@users.noreply.github.com>
2020-04-01 19:02:22 +0200

    Update bench_hy_bcast.c

--- bench_hy_bcast.c
+++ bench_hy_bcast.c
@@ -1,5 +1,5 @@
 /******************************************************************************************************************
-*       Title:          Micro-benchmark with broadcast based on the wrapper interfaces
+*       Title:          Micro-benchmark with hybrid MPI+MPI context-based broadcast based on the wrapper interfaces
 *       Date:           01/04/2020
 *       Authors:        Huan Zhou (huan.zhou@hlrs.de)
 *       Institute:      High Performance Computing Center Stuttgart (HLRS)
